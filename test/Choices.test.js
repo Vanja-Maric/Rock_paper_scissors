08ca5303e740f63fd3bcbbf86784eae7099570c3
Vanja-Maric <vm222hx@student.lnu.se>
2023-11-17 11:22:43 +0100

    RED available choices 0 = rock

--- test/Choices.test.js
+++ test/Choices.test.js
@@ -4,3 +4,8 @@ test("Choices should have avalviable choices with length 3", () => {
   const choices = new Choices();
   expect(choices.availableChoices.length).toBe(3);
 });
+
+test("Choices should have rock at index 1 in avalviable choices ", () => {
+  const choices = new Choices();
+  expect(choices.availableChoices[0]).toBe("rock");
+});
