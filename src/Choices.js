6cab9151af1f8cd2a1fba67054d52b6fb91af11d
Vanja-Maric <vm222hx@student.lnu.se>
2023-11-17 11:24:21 +0100

    RED available choices 1 = paper

--- src/Choices.js
+++ src/Choices.js
@@ -1,7 +1,7 @@
 export class Choices {
 
   constructor() {
-    this.availableChoices = ["", "", ""];
+    this.availableChoices = ["rock", "", ""];
   }
 
 }
--- test/Choices.test.js
+++ test/Choices.test.js
@@ -5,7 +5,12 @@ test("Choices should have avalviable choices with length 3", () => {
   expect(choices.availableChoices.length).toBe(3);
 });
 
-test("Choices should have rock at index 1 in avalviable choices ", () => {
+test("Choices should have rock at index 0 in avalviable choices ", () => {
   const choices = new Choices();
   expect(choices.availableChoices[0]).toBe("rock");
+});
+
+test("Choices should have paper at index 1 in avalviable choices ", () => {
+  const choices = new Choices();
+  expect(choices.availableChoices[0]).toBe("paper");
 });
