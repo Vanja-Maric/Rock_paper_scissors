55fc4de2f0714a6b655436a6eb220665ecfd70ec
Vanja-Maric <vm222hx@student.lnu.se>
2023-11-17 11:32:14 +0100

    REFACTOR

--- test/Choices.test.js
+++ test/Choices.test.js
@@ -19,3 +19,16 @@ test("Choices should have paper at index 2 in avalviable choices ", () => {
   const choices = new Choices();
   expect(choices.availableChoices[2]).toBe("scissors");
 });
+
+describe("Choices", () => {
+  const testCases = [
+    ["rock", 0],
+    ["paper", 1],
+    ["scissors", 2],
+  ];
+
+  test.each(testCases)("should have %p at index %i in available choices", (expectedChoice, index) => {
+    const choices = new Choices();
+    expect(choices.availableChoices[index]).toBe(expectedChoice);
+  });
+});
