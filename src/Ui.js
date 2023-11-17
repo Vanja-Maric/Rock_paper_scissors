214af3eca0d76b830c7e29391f4cda4586709383
Tea Sallstedt Elming <ts223ir@student.lnu.se>
2023-11-17 15:55:35 +0100

    GREEN

--- src/Ui.js
+++ src/Ui.js
@@ -1,6 +1,19 @@
 import readline from 'readline';
 export class Ui {
 
+  takeInInputFromTerminal(prompt) {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+
+    return new Promise((resolve) => {
+      rl.question(prompt, (input) => {
+        rl.close();
+        resolve(input);
+      });
+    });
+  }
 
 
 }
