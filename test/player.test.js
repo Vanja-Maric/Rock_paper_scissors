0dad4cc3ec255d5a7e66cf2f7c7826163541ebe5
Vanja-Maric <vm222hx@student.lnu.se>
2023-11-17 10:48:17 +0100

    RED setChoiceRock

--- test/player.test.js
+++ test/player.test.js
@@ -1,13 +1,19 @@
-import { Player } from"../src/Player.js";
+import { Player } from "../src/Player.js";
 
 describe('Player', () => {
-    describe.each([
-        ['Daniel'],
-        ['Tobias']
-    ])('with name %s', (name) => {
-        it('should create a player with the given name', () => {
-            const player = new Player(name);
-            expect(player.name).toBe(name);
-        });
+  describe.each([
+    ['Daniel'],
+    ['Tobias']
+  ])('with name %s', (name) => {
+    test('should create a player with the given name', () => {
+      const player = new Player(name);
+      expect(player.name).toBe(name);
     });
+  });
+});
+
+test('shouldAllowPlayerToSetChoiceRock', () => {
+  const player = new Player('Daniel');
+  player.setChoice('rock');
+  expect(player.choice).toBe('rock');
 });
