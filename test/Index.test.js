82154f69dc03482fb9db40fc4e5286d130a3ba2c
Tea Sallstedt Elming <ts223ir@student.lnu.se>
2023-12-01 12:25:45 +0100

    GREEN

--- test/Index.test.js
+++ test/Index.test.js
@@ -1,15 +1,26 @@
-import '..src/index.js';
+import { Game } from '../src/Game.js';
+import { Ui } from '../src/Ui.js';
 
-decsribe ('index', () => {
-  test('should call start function from game class', () => {
-    const mockStart = jest.fn();
-    Game.mockImplementation(() => {
-      return {
-        start: mockStart
-      };
-    });
-    index();
-    expect(mockStart).toHaveBeenCalled();
+jest.mock('../src/Ui.js', () => {
+  return jest.fn().mockImplementation(() => ({
+    greetingMessage: jest.fn(),
+    playMessage: jest.fn().mockReturnValue(true),
+    choiceInput: jest.fn(),
+    exitMessage: jest.fn(),
+    presentWinner: jest.fn()
+  }));
+});
+
+jest.spyOn(Game.prototype, 'start').mockImplementation(() => {});
+
+describe('index', () => {
+  beforeEach(() => {
+    Game.prototype.start.mockClear();
   });
-})
 
+  test('should call start function from Game class', () => {
+    require('../src/index.js');
+
+    expect(Game.prototype.start).toHaveBeenCalled();
+  });
+});
